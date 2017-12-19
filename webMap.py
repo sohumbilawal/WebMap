8657fcfd75f1d94b3bf01d6720dde1bee4c2fb47
sohumbilawal <sohumbilawal@gmail.com>
2017-12-19 15:48:45 -0800

    Stylized popups
    The popups for the markers now show the elevation for the corresponding volcano (data also from the csv file). Similar method used to latitudes and longitudes. There was an error, that popups cannot accept floats, or any non-string data. So changed el to string in the for loop itself. Also added m (meters) for readability.

--- webMap.py
+++ webMap.py
@@ -6,12 +6,14 @@ volData = pandas.read_csv("Volcanoes_USA.txt")
 lats = list(volData["LAT"])
 lons = list(volData["LON"])
 
-myMap=folium.Map(location = [34.094156, -118.341796], zoom_start=11, tiles = "Mapbox Bright")
+elev = list(volData["ELEV"])
+
+myMap=folium.Map(location = [34.094156, -118.341796], zoom_start=5, tiles = "Mapbox Bright")
 
 featGrp = folium.FeatureGroup(name = "My Map")
 
-for lat, lon in zip(lats, lons):
-    featGrp.add_child(folium.Marker(location=[lat, lon], popup = "Hi! I am a Marker", icon = folium.Icon(color='green')))
+for lat, lon, el in zip(lats, lons, elev):
+    featGrp.add_child(folium.Marker(location=[lat, lon], popup = str(el)+" m", icon = folium.Icon(color='green')))
 
 myMap.add_child(featGrp)
 
