0cacca0dcd8b8910371a048aacf81d60a871b3c2
sohumbilawal <sohumbilawal@gmail.com>
2017-12-19 15:59:20 -0800

    Stylized Markers
    These markers are colorized green, orange, or red based on the elevation of the volcanoes they are marking on the map.

--- webMap.py
+++ webMap.py
@@ -8,12 +8,20 @@ lons = list(volData["LON"])
 
 elev = list(volData["ELEV"])
 
+def elevCol(elevation):
+    if elevation < 1000.0:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 myMap=folium.Map(location = [34.094156, -118.341796], zoom_start=5, tiles = "Mapbox Bright")
 
 featGrp = folium.FeatureGroup(name = "My Map")
 
 for lat, lon, el in zip(lats, lons, elev):
-    featGrp.add_child(folium.Marker(location=[lat, lon], popup = str(el)+" m", icon = folium.Icon(color='green')))
+    featGrp.add_child(folium.Marker(location=[lat, lon], popup = str(el)+" m", icon = folium.Icon(color=elevCol(el))))
 
 myMap.add_child(featGrp)
 
