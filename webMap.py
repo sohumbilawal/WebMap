0bb8e9d0e0a69ffc0e00f2e29473e62e22f2bf9c
sohumbilawal <sohumbilawal@gmail.com>
2017-12-19 16:37:34 -0800

    The final commit
    Created 2 feature groups: one each for volcanoes and population-based colored polygons. This was done so that folium's LayerControl would detefct each of those two map features as an individual layer, which you cantoggle on or off.

--- webMap.py
+++ webMap.py
@@ -18,13 +18,18 @@ def elevCol(elevation):
 
 myMap=folium.Map(location = [34.094156, -118.341796], zoom_start=5, tiles = "Mapbox Bright")
 
-featGrp = folium.FeatureGroup(name = "My Map")
+featGrpV = folium.FeatureGroup(name = "Volcano Positions (USA)")
 
 for lat, lon, el in zip(lats, lons, elev):
-    featGrp.add_child(folium.CircleMarker(location=[lat, lon], radius = 5,  fill_color=elevCol(el), color = 'black', fill = True, fill_opacity = 0.8, popup = str(el)+" m"))
+    featGrpV.add_child(folium.CircleMarker(location=[lat, lon], radius = 5,  fill_color=elevCol(el), color = 'black', fill = True, fill_opacity = 0.8, popup = str(el)+" m"))
 
-featGrp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding = 'utf-8-sig').read(), style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+featGrpP = folium.FeatureGroup(name = "National population data")
 
-myMap.add_child(featGrp)
+featGrpP.add_child(folium.GeoJson(data=open('world.json', 'r', encoding = 'utf-8-sig').read(), style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+myMap.add_child(featGrpV)
+myMap.add_child(featGrpP)
+
+myMap.add_child(folium.LayerControl())
 
 myMap.save("map1.html")
